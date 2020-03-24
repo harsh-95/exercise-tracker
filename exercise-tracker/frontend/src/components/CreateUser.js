e33c3d656ea81b05089b82e43eb7f7c12dd309f4
Harsh Gupta <harshgupta951114@gmail.com>
2020-03-24 13:54:54 +0530

    create user UI

--- exercise-tracker/frontend/src/components/CreateUser.js
+++ exercise-tracker/frontend/src/components/CreateUser.js
@@ -1,9 +1,46 @@
 import React, { Component } from "react";
 
 class CreateUser extends Component{
+
+    constructor(props){
+        super(props);
+
+        this.state = {
+            username: ''
+        }
+    }
+
+    onChangeUsername = (e)=>{
+        this.setState({
+            username: e.target.value
+        })
+    }
+
+    onSubmit = (e)=>{
+        e.preventDefault();
+
+        const user = this.state;
+        console.log(user);
+    }
+
     render(){
         return(
-            <div>Create User!</div>
+            <div>
+                <h3>Create Exercise</h3>
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-8">
+                            <form class="add-exercise-form" onSubmit={this.onSubmit} action="">
+                                <div className="form-group">
+                                    <label for="username">Username:</label>
+                                    <input type="text" name="username" id="username" onChange={this.onChangeUsername} value={this.state.username} className="form-control"/>
+                                </div>
+                                <input type="submit" className="btn btn-sucess" value="Submit"/>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
         );
     }
 }
